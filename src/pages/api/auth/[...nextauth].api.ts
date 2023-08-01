4d2c70af765cfbc2dbbd6615e4a6b9a66ebc92a0
期贤 <qixian.cs@antgroup.com>
2023-08-01 09:58:42 +0800

    remove code

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || 'Iv1.ef859671fdf9841a',
-      clientSecret: process.env.GITHUB_SECRET || '98cb757a9515c76c89fabd72b63b23bb2d35bc7a',
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
 };
