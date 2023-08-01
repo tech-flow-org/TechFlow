2b97568654207ab7702500a06709aded9c03b541
期贤 <qixian.cs@antgroup.com>
2023-08-01 17:23:14 +0800

    support github login

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -2,6 +2,7 @@
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.AUTH_GITHUB_ID || '',
