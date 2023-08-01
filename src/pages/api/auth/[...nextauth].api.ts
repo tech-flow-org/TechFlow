09eeaa37e6a16442da5fe0cde802b467f4080906
期贤 <qixian.cs@antgroup.com>
2023-08-01 10:34:18 +0800

    优化代码逻辑

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,7 +1,7 @@
 ﻿import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
