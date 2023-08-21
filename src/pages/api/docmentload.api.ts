c1448cd2e4423586d95fbddfd2bea70bc489b581
期贤 <qixian.cs@antgroup.com>
2023-08-21 16:29:33 +0800

    增加文档 loading 和  embedding 接口

---
+++ src/pages/api/docmentload.api.ts
@@ -0,0 +1,50 @@
+﻿import { OPENAI_SERVICE_ERROR_CODE } from '@/const/fetch';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { TokenTextSplitter } from 'langchain/text_splitter';
+
+export const config = {
+  runtime: 'edge',
+};
+
+type DocumentsLoadPayload = {
+  documents: string;
+};
+
+export default async function handler(request: Request) {
+  const payload = (await request.json()) as DocumentsLoadPayload;
+
+  const splitter = new TokenTextSplitter({
+    encodingName: 'gpt2',
+    chunkSize: 200,
+    chunkOverlap: 0,
+  });
+
+  const output = await splitter.createDocuments([payload.documents]);
+
+  let documents = [];
+
+  const embeddings = new OpenAIEmbeddings({
+    timeout: 1000, // 1s timeout
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  for await (const document of output) {
+    documents.push({
+      ...document,
+      embeddings: await embeddings.embedQuery(document.pageContent),
+    });
+  }
+
+  try {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data: { output: documents },
+      }),
+    );
+  } catch (e) {
+    const error = JSON.stringify((e as Error).cause);
+
+    return new Response(error, { status: OPENAI_SERVICE_ERROR_CODE, statusText: error });
+  }
+}
---
+++ src/pages/api/embeddings.api.ts
@@ -0,0 +1,37 @@
+﻿import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+
+import { OPENAI_SERVICE_ERROR_CODE } from '@/const/fetch';
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing env var from OpenAI');
+}
+export const config = {
+  runtime: 'edge',
+};
+
+type OpenAIEmbeddingsPayload = {
+  query: string;
+};
+
+export default async function handler(request: Request) {
+  const payload = (await request.json()) as OpenAIEmbeddingsPayload;
+  const embeddings = new OpenAIEmbeddings({
+    timeout: 1000, // 1s timeout
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const res = await embeddings.embedQuery(payload.query);
+
+  try {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data: { output: res },
+      }),
+    );
+  } catch (e) {
+    const error = JSON.stringify((e as Error).cause);
+
+    return new Response(error, { status: OPENAI_SERVICE_ERROR_CODE, statusText: error });
+  }
+}
