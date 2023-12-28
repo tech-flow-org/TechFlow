6a3980bc9ff40d01ec3cdbff500d5abaa1fdb495
期贤 <qixian.cs@antgroup.com>
2023-12-28 11:20:03 +0800

    增加 github 答疑功能

---
+++ src/pages/api/docantd.api.ts
@@ -0,0 +1,59 @@
+﻿import { QdrantClient } from '@qdrant/js-client-rest';
+import { NextApiRequest, NextApiResponse } from 'next';
+import OpenAI from 'openai';
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing env var from OpenAI');
+}
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const qdrantClient = new QdrantClient({
+  url: process.env.QDRANT_API_URL,
+  apiKey: process.env.QDRANT_API_KEY,
+});
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const payload = (await request.body) as {
+    body: string;
+    title: string;
+  };
+
+  const embedding = await openai.embeddings
+    .create({
+      input: payload.title,
+      model: 'text-embedding-ada-002',
+    })
+    .then((res) => {
+      return res.data.at(0)?.embedding;
+    });
+
+  const searchResult = await qdrantClient.search('test_collection', {
+    vector: embedding || [],
+    limit: 3,
+  });
+
+  const chatData = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-16k',
+    messages: [
+      {
+        role: 'system',
+        content: ` 你是一个专业的前端答疑专家，你需要基于前端知识来做出让初学者都能听懂的回答`,
+      },
+      {
+        role: 'user',
+        content: `基于以下片段，回答 ${payload.title} 的问题
+${searchResult.map((item) => `- ${item?.payload?.text}`).join('\n')})}`,
+      },
+    ],
+    stream: false,
+    temperature: 0.9,
+  });
+
+  return response.send(
+    JSON.stringify({
+      message: chatData.choices[0]?.message?.content,
+      success: true,
+    }),
+  );
+}
---
+++ src/pages/api/github.api.ts
@@ -0,0 +1,55 @@
+﻿import { Octokit } from '@octokit/rest';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const { GITHUB_TOKEN } = process.env;
+
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+});
+
+export default async function handler(request: NextApiRequest, res: NextApiResponse) {
+  if (request.method === 'GET') {
+    const { owner, repo, page, pageSize } = (await request.query) as unknown as {
+      owner: string;
+      repo: string;
+      page: number;
+      pageSize: number;
+    };
+    const data = await octokit
+      .paginate(
+        octokit.rest.issues.listForRepo,
+        {
+          owner,
+          repo,
+          since: '2023-01-01',
+          state: 'open',
+          sort: 'created',
+          page,
+          per_page: pageSize * page,
+        },
+        (response) => response.data,
+      )
+      .then((issues) => {
+        return issues.map((issue) => {
+          return {
+            title: issue.title,
+            id: issue.id,
+            body: issue.body,
+          };
+        });
+      });
+
+    return res.send(data);
+  }
+
+  if (request.method === 'POST') {
+    const { owner, repo, issue_number, body } = request.body;
+    const data = await octokit.rest.issues.createComment({
+      repo,
+      owner,
+      issue_number,
+      body,
+    });
+    return res.send(data);
+  }
+}
