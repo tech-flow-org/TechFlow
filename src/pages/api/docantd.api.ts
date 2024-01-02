a7581bf2923877453b42fd653a4c9f3f19ec9de8
期贤 <qixian.cs@antgroup.com>
2024-01-02 19:27:10 +0800

    修复信息

--- src/pages/api/docantd.api.ts
+++ src/pages/api/docantd.api.ts
@@ -47,15 +47,25 @@ export default async function handler(request: Request) {
           messages: [
             {
               role: 'system',
-              content: ` 你是一个专业的前端答疑专家，你需要基于前端知识来做出让初学者都能听懂的回答，
-              并且保证技术都是先进的，比如样式修改要用
-              - DesginToken 而不是使用 less
-              - react  要用 hooks
-              - 不推荐使用 dva 和  redux`,
+              content: ` 你是一个专业的前端答疑专家，你需要基于前端知识来做一步步的给出解决方案，请根据以下问题来回答：
+我们需要你基于以下知识来回答：
+  - 前端知识
+  - 前端开发流程
+  - 前端工程化
+  - 前端性能优化
+  - React  技术
+  - Umi  和  antd
+请注意，我们需要你对前端技术有一定的了解，并且能够在短时间内给出一个解决方案，并且保证技术都是先进的，比如:
+  - 样式修改要用 Design Token 而不是使用 less
+  - react  要用 hooks
+  - 不推荐使用 redux
+  `,
             },
             {
               role: 'user',
-              content: `基于以下片段，简明扼要的回复 ${payload.title + payload.body} 的问题
+              content: `基于以下片段，简明扼要的一步步的给出${
+                payload.title + payload.body
+              } 问题的解决方案 
   ${searchResult.map((item) => `- ${item?.payload?.text}`).join('\n')})}`,
             },
           ],
@@ -64,7 +74,11 @@ export default async function handler(request: Request) {
         });
 
         for await (const part of chatData) {
-          controller.enqueue(encoder.encode(part.choices[0]?.delta?.content || ''));
+          controller.enqueue(`> 值得注意是的百分之二十的问题都可以用重装依赖来解决，所以你可以尝试一下：
+ 删除 'node_modules' 文件夹 -> 删除 'package-lock.json'或 'pnpm-lock.yaml' 文件 -> 运行 'pnpm install' 或  'npm install' 命令
+
+ 
+${encoder.encode(part.choices[0]?.delta?.content || '')}`);
         }
       },
     }),
